110765ab4bbc7621d0d5f2ceb8951bf7c0b1e296
Dai Jie Liu <20087611@mail.wit.ie>
2021-12-09 23:05:38 +0000

    Mongo lab Error Handling

--- movies-api/api/users/index.js
+++ movies-api/api/users/index.js
@@ -13,32 +13,23 @@ router.get('/', async (req, res) => {
 });
 
 // register
-router.post('/',asyncHandler( async (req, res, next) => {
-    if (!req.body.username || !req.body.password) {
-      res.status(401).json({success: false, msg: 'Please pass username and password.'});
-    }
-    const regex = new RegExp('^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{5,}$')
-    if (req.query.action === 'register' && regex.test(req.body.password)==false) {
-      await User.create(req.body).catch(next);
-      res.status(201).json({code: 201, msg: 'Successful created new user.'     });
-    } else {
-        if(regex.test(req.body.password)==true){
-            res.status(401).json({code: 401,msg: 'Incorrect Password Format'})
-        }
-      const user = await User.findByUserName(req.body.username).catch(next);
-        if (!user) return res.status(401).json({ code: 401, msg: 'Authentication failed. User not found.' });
-        user.comparePassword(req.body.password, (err, isMatch) => {
-          if (isMatch && !err) {
-            // if user is found and password matches, create a token
-            const token = jwt.sign(user.username, process.env.SECRET);
-            // return the information including token as JSON
-            res.status(200).json({success: true, token: 'BEARER ' + token});
-          } else {
-            res.status(401).json({code: 401,msg: 'Authentication failed. Wrong password.'});
-          }
+router.post('/', asyncHandler(async (req, res) => {
+    if (req.query.action === 'register') {  //if action is 'register' then save to DB
+        await User(req.body).save()
+        res.status(201).json({
+            code: 201,
+            msg: 'Successful created new user.',
         });
-      }
-  }));  
+    }
+    else {  //Must be authenticating the!!! Query the DB and check if there's a match
+        const user = await User.findOne(req.body);
+        if (!user) {
+            return res.status(401).json({ code: 401, msg: 'Authentication failed' })
+        } else {
+            return res.status(200).json({ code: 200, msg: "Authentication Successful", token: 'TEMPORARY_TOKEN' })
+        }
+    }
+})); 
 
  // Update a user
  router.put('/:id', async (req, res) => {
@@ -69,14 +60,11 @@ router.post('/:userName/favourites', asyncHandler(async (req, res) => {
     res.status(201).json(user); 
   }));
 
-  router.get('/:id/favourites', async (req, res) => {
-    const user = await User.findById(req.params.id);
-    if (user) {
-        res.status(200).json(user.favourites);
-    } else {
-        res.status(404).json({ code: 404, msg: 'Unable to find favourites' });
-    }
-});
+  router.get('/:userName/favourites', asyncHandler( async (req, res) => {
+    const userName = req.params.userName;
+    const user = await User.findByUserName(userName).populate('favourites');
+    res.status(201).json(user.favourites);
+  }));
 
 
 export default router;
