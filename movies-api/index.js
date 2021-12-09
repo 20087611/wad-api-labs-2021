4addd815cca4bf9772c306df148626d78d68cf78
Dai Jie Liu <20087611@mail.wit.ie>
2021-12-09 22:59:14 +0000

    Mongo lab Users Router

--- movies-api/index.js
+++ movies-api/index.js
@@ -1,6 +1,8 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import moviesRouter from './api/movies';
+import './db';
+import usersRouter from './api/users';
 
 dotenv.config();
 
@@ -9,6 +11,8 @@ const app = express();
 const port = process.env.PORT;
 
 app.use(express.json());
+
+app.use('/api/users', usersRouter);
 
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
